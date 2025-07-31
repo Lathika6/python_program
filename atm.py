0662b597d0c991563e06d9e00be4a2a62b46aa9d
Lathika6 <Lathikasreek19@gmail.com>
2025-07-31 15:25:14 +0530

    Create atm.py

---
+++ atm.py
@@ -0,0 +1,22 @@
+lathika.py
+print("select the services you want:")
+print('1.withdraw')
+print('2.deposit \n 3. savings\4. balance')
+service=int(input('plz select:'))
+if service==1:
+    print('withdraw')
+    amount=int(input('enter a amount:'))
+    if pin=PIN
+    print('collect your amount ',amount)
+    else:
+        print('incorrect pin')
+        status=true
+        run=false
+elif service==2:
+    print('deposit')
+elif service==3:
+    print('savings')
+elif service==4:
+    print('balance')
+else:
+    print('kindly select the above service')
